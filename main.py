6a5803d56f5e87b8fc3c4e9f2a88da75202a3b09
Brian Faure <bfaure23@gmail.com>
2018-05-29 19:54:47 -0400

    first magnet link is now parsed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import urllib2
 
 
-def get_results(title):
+def get_html(title):
 	hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
 	       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 	       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -22,4 +22,12 @@ def get_results(title):
 	    print e.fp.read()
 	    return None
 
-print get_results('of mice and men')
+def get_magnet_link(title):
+	html=get_html(title)
+	start_tag="<a href=\"magnet"
+	start_idx=html.find(start_tag)
+	end_idx=html.find("\"",start_idx+len(start_tag))
+	return html[start_idx+9:end_idx]
+
+
+print get_magnet_link('of mice and men')
